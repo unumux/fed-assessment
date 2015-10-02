41e6cfa91bc070ce3186bc19f41d2e1f9e1cb16b
Kyle Wagner <Keale2@gmail.com>
2015-10-02 13:12:36 -0400

    I don't know what I did.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("styles/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
+// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-	gulp.src('/styles/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./css'));
+    return gulp.src("styles/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./dist"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('sass:watch', function() {
-	gulp.watch('./styles/*.scss', ['sass']);
-});
+gulp.task('default', ['serve']);
